72471426918c92328872d1ba75096ec1e804293c
Sri Pranav Kunda <sripkunda@gmail.com>
2025-03-17 16:10:10 -0700

    Committed from machine for training neural net.

--- src/experiment_utils.py
+++ src/experiment_utils.py
@@ -8,4 +8,19 @@ def generate_simulation_data(K=10):
         terms = [(A[k] * np.sin(2 * np.pi * (k+1) * t) + B[k] * np.cos(2 * np.pi * (k+1) * t)) / (k+1)
                 for k in range(K)]
         return sum(terms)
-    return generate_signal
+    return generate_signal, A, B
+
+def generate_legendre_basis_expansion(K=4):
+    np.random.seed(123)
+    coeffs = np.random.randn(K)
+    return np.polynomial.legendre.Legendre(coeffs, domain=[0, 1]), coeffs
+
+def get_legendre_polynomial(i, K=4):
+    coeffs = np.zeros(K)
+    coeffs[i] = 1
+    return np.polynomial.legendre.Legendre(coeffs, domain=[0, 1])
+
+def multiply_functions(f, g):
+    def product(t):
+        return f(t) * g(t)
+    return product
